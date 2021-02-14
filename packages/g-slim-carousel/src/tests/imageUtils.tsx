59ff3c5dc846d0ebd77c6bad533958609da38c33
gangsthub <paulmelero@gmail.com>
2021-02-14 23:43:56 +0100

    update API

--- packages/g-slim-carousel/src/tests/imageUtils.tsx
+++ packages/g-slim-carousel/src/tests/imageUtils.tsx
@@ -5,19 +5,31 @@ export const getRandomInNumericRange = (max: number, min = 0): number => {
   return Math.floor((max - min) * Math.random()) + min;
 };
 
-export const getRandomListOfImages = (
+export const getImageSource = ({
+  width,
+  height,
+  index = 0,
+}: {
+  width: number;
+  heigth: number;
+  index?: number;
+}): string => `https://picsum.photos/id/${1001 + index}/${width}/${height}.webp`;
+
+export const getRandomListOfImages = ({
   numberOfImages = 1,
   width = 1300,
-  height = parseInt(DEFAULT_OPTIONS.minHeight)
-): ReactNode => {
+  height = parseInt(DEFAULT_OPTIONS.minHeight),
+}: {
+  numberOfImages?: number;
+  width?: number;
+  height?: number;
+}): ReactNode => {
   return Array(numberOfImages)
     .fill()
     .map((_, index) => (
       <img
         key={index}
-        src={`https://picsum.photos/id/${1000 + index}/${width}/${height}.webp`}
-        loading='lazy'
-        alt=''
+        src={getImageSource({ width, height, index })}
         style={{ objectFit: 'cover', objectPosition: '50% 50%' }}
       />
     ));
